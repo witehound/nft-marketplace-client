2e585a6197c10c92d173794f72bcd8903956072c
witehound <netvid00@gmail.com>
2023-01-12 22:02:41 +0100

    finished html layout for the notification component

--- components/Navbar/Notification/Notification.jsx
+++ components/Navbar/Notification/Notification.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import Image from "next/image";
+
+//internal imports
 import styles from "./Notification.module.css";
+import images from "../../../img";
 
 const Notification = () => {
-  return <div>Notification</div>;
+  return (
+    <div className={styles.notification}>
+      <p>Notification</p>
+      <div className={styles.notification_box}>
+        <div className={styles.notification_box_image}>
+          <Image
+            src={images.user1}
+            alt="profile image"
+            width={50}
+            height={50}
+            className={styles.notification_box_image}
+          />
+        </div>
+        <div className={styles.notification_box_info}>
+          <h4>Winegd Anubis</h4>
+          <p>Measure Action your user...</p>
+          <small>3 minutes ago</small>
+        </div>
+        <span className={styles.notification_box_new}></span>
+      </div>
+    </div>
+  );
 };
 
 export default Notification;
