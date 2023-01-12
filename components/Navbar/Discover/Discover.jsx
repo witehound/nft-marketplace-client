6eddf16905c726f128fb1a80a81ea399f3663801
witehound <netvid00@gmail.com>
2023-01-12 21:42:39 +0100

    finished html layout for the discover component

--- components/Navbar/Discover/Discover.jsx
+++ components/Navbar/Discover/Discover.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import Link from "next/link";
+
+//internnal import
 import styles from "./Discover.module.css";
 
 const Discover = () => {
-  return <div>Discover</div>;
+  const discover = [
+    {
+      name: "Collection",
+      link: "collection",
+    },
+    {
+      name: "Search",
+      link: "search",
+    },
+    {
+      name: "Author Profile",
+      link: "author-profile",
+    },
+    {
+      name: "NFT Details",
+      link: "nft-details",
+    },
+    {
+      name: "Account Settings",
+      link: "account-settings",
+    },
+    {
+      name: "Connect Wallet",
+      link: "connect-wallet",
+    },
+    {
+      name: "Blog",
+      link: "blog",
+    },
+  ];
+  return (
+    <>
+      <div>
+        {discover.map((el, i) => (
+          <div key={i + 1} className={styles.discover}>
+            <Link href={{ pathname: el.link }}>{el.name}</Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Discover;
