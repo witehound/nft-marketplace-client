001f4a0c8c03086527f559cdd38428e232978d0b
witehound <netvid00@gmail.com>
2023-01-13 18:54:18 +0100

    completed css styling for footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,9 +1,73 @@
 import React from "react";
+import Image from "next/image";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialInstagram,
+} from "react-icons/ti";
+import { RiSendPlaneFill } from "react-icons/ri";
 
-import Style from "./Footer.module.css";
+//internal imports
+import styles from "./Footer.module.css";
+import images from "../../img";
+import { Discover, HelpCenter } from "../Navbar";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footer_box}>
+        <div className={styles.footer_box_social}>
+          <Image src={images.logo} alt="footer logo" height={100} width={100} />
+          <p>
+            The world’s first and largest digital marketplace for crypto
+            collectibles and non-fungible tokens (NFTs). Buy, sell, and discover
+            exclusive digital items.
+          </p>
+          <div className={styles.footer_social}>
+            <a href="#">
+              <TiSocialFacebook />
+            </a>
+            <a href="#">
+              <TiSocialLinkedin />
+            </a>
+            <a href="#">
+              <TiSocialTwitter />
+            </a>
+            <a href="#">
+              <TiSocialYoutube />
+            </a>
+            <a href="#">
+              <TiSocialInstagram />
+            </a>
+          </div>
+        </div>
+        <div className={styles.footer_box_discover}>
+          <h3>Discover</h3>
+          <Discover />
+        </div>
+        <div className={styles.footer_box_help}>
+          <h3>Help Center</h3>
+          <HelpCenter />
+        </div>
+        <div className={styles.subscribe}>
+          <h3>Subscribe</h3>
+
+          <div className={styles.subscribe_box}>
+            <input type="email" placeholder="Enter youremail" />
+            <RiSendPlaneFill className={styles.subscribe_box_send} />
+          </div>
+          <div className={styles.subscribe_box_info}>
+            <p>
+              Discover, collect, and sell extraordinary NFTs OpenSea is the
+              world first and largest NFT marketplace
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
