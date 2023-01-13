5199596a28c28cc0d9274b7f2590a68e4c3ca366
witehound <netvid00@gmail.com>
2023-01-13 12:33:54 +0100

    all changees before 13th january

--- components/Navbar/Sidebar/Sidebar.jsx
+++ components/Navbar/Sidebar/Sidebar.jsx
@@ -1,8 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { GrClose } from "react-icons/gr";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialInstagram,
+  TiArrowSortedDown,
+  TiArrowSortedUp,
+} from "react-icons/ti";
+
+//internal import
 import styles from "./Sidebar.module.css";
+import images from "../../../img";
+import Button from "../../Button/Button";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [openDiscover, setOpenDiscover] = useState(false);
+  const [openHelp, setOpenHelp] = useState(false);
+
+  const discover = [
+    {
+      name: "Collection",
+      link: "collection",
+    },
+    {
+      name: "Search",
+      link: "search",
+    },
+    {
+      name: "Author Profile",
+      link: "author-profile",
+    },
+    {
+      name: "NFT Details",
+      link: "nft-details",
+    },
+    {
+      name: "Account Settings",
+      link: "account-settings",
+    },
+    {
+      name: "Connect Wallet",
+      link: "connect-wallet",
+    },
+    {
+      name: "Blog",
+      link: "blog",
+    },
+  ];
+  const helpCenter = [
+    {
+      name: "About",
+      link: "about",
+    },
+    {
+      name: "Contact Us",
+      link: "contact-us",
+    },
+    {
+      name: "Sign Up",
+      link: "sign-up",
+    },
+    {
+      name: "Sign In",
+      link: "sign-in",
+    },
+    {
+      name: "Subscription",
+      link: "subscription",
+    },
+  ];
+  return <></>;
 };
 
 export default Sidebar;
